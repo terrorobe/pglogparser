389f1924a007e64ad807cdcd38d5f1da95e135ae
Michael Renner <michael.renner@amd.co.at>
2009-08-31 01:00:03 +0200

    small changes

--- pg_statementlog_parser.pl
+++ pg_statementlog_parser.pl
@@ -11,17 +11,20 @@ my $pglogdurationre = qr!
                 duration:\s+([\d.]+)\s+ms # Diratuion
                 !x;
 
-my $threshold = 10;
+my $threshold = 10; # msec
 
 my $backend    = 0;
 my $linenumber = 0;
 my $printme    = 0;
 
-while ( my $line = <STDIN> ) {
+while ( my $line = <ARGV> ) {
 
     if ( $line =~ m/$pglogchunkre/ ) {
 
         my ( $curbackend, $curlinenumber, $content ) = ( $1, $2, $3 );
+
+# If this is the first line number of a chunk and it contains a duration
+# above the threshold then log it
 
         if ( $curlinenumber == 1 ) {
             if ( $content =~ m/$pglogdurationre/ ) {
@@ -31,10 +34,18 @@ while ( my $line = <STDIN> ) {
                 }
             }
         }
+
+# If the backend number hasn't been changed, this needs to get printed
         elsif ( $curbackend == $backend ) {
             $printme = 1;
         }
+
+# If it's neither a starting or a continuous line, reset backend for good
+        else {
+            $backend = -1;
+        }
     }
+
     if ($printme) {
         print $line;
     }
