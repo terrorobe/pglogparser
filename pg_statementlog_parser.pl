b7e9b6a3484d938312e747b3a013971eda8e3651
Michael Renner <michael.renner@amd.co.at>
2009-09-01 13:45:55 +0200

    fixfixfix

--- pg_statementlog_parser.pl
+++ pg_statementlog_parser.pl
@@ -8,10 +8,10 @@ my $pglogchunkre = qr!\[(\d+)-(\d+)\]\s+(.*)!x;
 my $pglogdurationre = qr!
                 .*?                     # Timestamp
                 LOG:\s+                 # Log identifier
-                duration:\s+([\d.]+)\s+ms # Diratuion
+                duration:\s+([\d.]+)\s+ms # Duration
                 !x;
 
-my $threshold = 10; # msec
+my $threshold = 1000; # msec
 
 my $backend    = 0;
 my $linenumber = 0;
