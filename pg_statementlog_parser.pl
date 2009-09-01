258d0055f410d0290b8a322b3f38111cd0c07b16
Michael Renner <michael.renner@amd.co.at>
2009-09-01 14:02:16 +0200

    refactor refactor

--- pg_statementlog_parser.pl
+++ pg_statementlog_parser.pl
@@ -3,41 +3,53 @@
 use strict;
 use warnings;
 
-my $pglogchunkre = qr!\[(\d+)-(\d+)\]\s+(.*)!x;
+my $pglogchunkre = qr!\[(\d+)-(\d+)\]\s(.*)!x;
 
 my $pglogdurationre = qr!
-                .*?                     # Timestamp
+                (.*?)\s+                # Timestamp
                 LOG:\s+                 # Log identifier
-                duration:\s+([\d.]+)\s+ms # Duration
+                duration:\s+([\d.]+)\s+ms\s+ # Duration
+                statement:\s+(.*)       # Statement
                 !x;
 
 my $threshold = 1000; # msec
 
 my $backend    = 0;
 my $linenumber = 0;
-my $printme    = 0;
+my $statementbuf = '';
+my $timestamp = '';
+my $duration = 0;
 
 while ( my $line = <ARGV> ) {
 
     if ( $line =~ m/$pglogchunkre/ ) {
 
-        my ( $curbackend, $curlinenumber, $content ) = ( $1, $2, $3 );
+        my ( $curbackend, $curlinenumber, $content ) = ( $1, $2, $3, );
 
 # If this is the first line number of a chunk and it contains a duration
 # above the threshold then log it
 
         if ( $curlinenumber == 1 ) {
+
+            if ($statementbuf) {
+                print "$timestamp: $duration ms: $statementbuf\n";
+                $statementbuf = '';
+            }
+
             if ( $content =~ m/$pglogdurationre/ ) {
-                if ( $1 > $threshold ) {
+                my ( $statementpart );
+                ( $timestamp, $duration, $statementpart ) = ( $1, $2, $3 );
+
+                if ( $duration > $threshold ) {
                     $backend = $curbackend;
-                    $printme = 1;
+                    $statementbuf = $statementpart;
                 }
             }
         }
 
 # If the backend number hasn't been changed, this needs to get printed
         elsif ( $curbackend == $backend ) {
-            $printme = 1;
+            $statementbuf .= $content;
         }
 
 # If it's neither a starting or a continuous line, reset backend for good
@@ -45,9 +57,9 @@ while ( my $line = <ARGV> ) {
             $backend = -1;
         }
     }
+}
 
-    if ($printme) {
-        print $line;
-    }
-    $printme = 0;
+
+if ($statementbuf) {
+    print "$timestamp: $duration ms: $statementbuf\n";
 }
